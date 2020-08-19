3a8adfad6fca65d2cd7cf118072be87191d8710c
liaosipei <liaosipei@corp.netease.com>
2020-08-19 17:14:52 +0800

    [版本更新] 更新SDK/podspec至5.7.3版本

--- SDK/ExportHeaders/QYSDK.h
+++ SDK/ExportHeaders/QYSDK.h
@@ -2,7 +2,7 @@
 //  QYSDK.h
 //  QYSDK
 //
-//  version 5.7.2
+//  version 5.7.3
 //
 //  Created by towik on 12/21/15.
 //  Copyright (c) 2017 Netease. All rights reserved.
@@ -15,6 +15,11 @@
  *  完成回调
  */
 typedef void(^QYCompletionBlock)(BOOL success);
+
+/**
+ *  完成回调，带错误参数
+ */
+typedef void(^QYResultCompletionBlock)(BOOL success, NSError *error);
 
 /**
  *  完成结果回调
@@ -30,6 +35,18 @@ typedef void(^QYPushMessageBlock)(QYPushMessage *pushMessage);
  *  清理缓存回调
  */
 typedef void(^QYCleanCacheCompletion)(NSError *error);
+
+/**
+ *  错误码
+ */
+typedef NS_ENUM(NSInteger, QYLocalErrorCode) {
+    QYLocalErrorCodeUnknown         = 0,    //未知错误
+    QYLocalErrorCodeInvalidParam    = 1,    //错误参数
+    QYLocalErrorCodeFusionNeeded    = 2,    //必须为融合SDK
+    QYLocalErrorCodeAccountNeeded   = 3,    //帐号错误-底层通信IM帐号未登录
+    QYLocalErrorCodeInvalidUserId   = 4,    //userId错误，应与帐号相同
+    QYLocalErrorCodeNeedLogout      = 5,    //userId变化，应走帐号切换逻辑，先调用logout
+};
 
 
 /**
@@ -51,6 +68,13 @@ typedef void(^QYCleanCacheCompletion)(NSError *error);
  *  @param appName  对应管理后台添加一个App时填写的App名称 (就是SDK1.0.0版本的cerName,参数名变动)
  */
 - (void)registerAppId:(NSString *)appKey appName:(NSString *)appName;
+
+/**
+ *  返回AppKey
+ *
+ *  @return appKey
+ */
+- (NSString *)appKey;
 
 /**
  *  返回客服聊天ViewController，必须嵌入至UINavigationController中
@@ -82,39 +106,77 @@ typedef void(^QYCleanCacheCompletion)(NSError *error);
 - (QYConversationManager *)conversationManager;
 
 /**
+ *  获取当前设置的用户信息ID
+ *
+ *  @return 返回当前已设置的用户信息ID
+ */
+- (NSString *)currentUserID;
+
+/**
+ *  获取当前登录的底层IM通信帐号ID，可据此判断当前是否已登录IM帐号
+ *
+ *  @return 返回当前登录的accountID
+ *  @discussion 此帐号与外部设置的userId不同，是底层长连接通信帐号，七鱼内部会自动创建并登录；与外部传入的userId形成映射关系
+ */
+- (NSString *)currentIMAccountID;
+
+/**
+ *  设置用户信息，App帐号登录成功后上传
+ *
+ *  @param userInfo 用户信息
+ *  @discussion 此方法尽量在App帐号登录成功后调用，不应仅在进入客服界面时调用；否则可能会造成客服连接状态不稳定
+ *  @discussion 若设置的userId与上次设置不同，即需要实现帐号切换，应先调用logout还原为匿名帐号再进行设置
+ */
+- (void)setUserInfo:(QYUserInfo *)userInfo;
+
+/**
+ *  设置用户信息，App帐号登录成功后上传，带结果回调
+ *
+ *  @param userInfo 用户信息
+ *  @param userInfoBlock userInfo上报结果回调
+ *  @discussion 此方法尽量在App帐号登录成功后调用，不应仅在进入客服界面时调用；否则可能会造成客服连接状态不稳定
+ *  @discussion 若设置的userId与上次设置不同，即需要实现帐号切换，应先调用logout还原为匿名帐号再进行设置
+ */
+- (void)setUserInfo:(QYUserInfo *)userInfo userInfoResultBlock:(QYResultCompletionBlock)userInfoBlock;
+
+/**
+ *  注销当前帐号，App帐号注销时调用
+ *
+ *  @param completion 完成回调
+ *  @discussion 切换帐号包含注销和登录过程，在注销阶段也要调用此函数
+ */
+- (void)logout:(QYCompletionBlock)completion;
+
+/**
+ *  设置待校验的authToken
+ */
+- (void)setAuthToken:(NSString *)authToken;
+
+/**
+ *  设置用户信息，App帐号登录成功后上传，带authToken校验
+ *
+ *  @param userInfo 用户信息
+ *  @param block authToken校验结果回调
+ */
+- (void)setUserInfo:(QYUserInfo *)userInfo authTokenVerificationResultBlock:(QYCompletionWithResultBlock)block;
+
+/**
+ *  设置用户信息，App帐号登录成功后上传，带authToken校验，带结果回调
+ *
+ *  @param userInfo 用户信息
+ *  @param userInfoBlock userInfo上报结果回调
+ *  @param authTokenBlock authToken校验结果回调
+ *  @discussion 此方法尽量在App帐号登录成功后调用，不应仅在进入客服界面时调用；否则可能会造成客服连接状态不稳定
+ *  @discussion 若设置的userId与上次设置不同，即需要实现帐号切换，应先调用logout还原为匿名帐号再进行设置
+ */
+- (void)setUserInfo:(QYUserInfo *)userInfo userInfoResultBlock:(QYResultCompletionBlock)userInfoBlock authTokenResultBlock:(QYCompletionBlock)authTokenBlock;
+
+/**
  *  更新推送token
  *
  *  @param token 推送token
  */
 - (void)updateApnsToken:(NSData *)token;
-
-/**
- *  注销当前账号。用户帐号注销时需要调用此函数；切换账号包含注销和登录过程，在注销阶段也要调用此函数
- *
- *  @param completion 完成回调
- */
-- (void)logout:(QYCompletionBlock)completion;
-
-/**
- *  设置个人信息。用户帐号登录成功之后，调用此函数
- *  注意：此方法尽量在账号登录成功后调用，而不应仅在进入客服界面时调用；否则可能会造成客服连接状态不稳定
- *
- *  @param userInfo 个人信息
- */
-- (void)setUserInfo:(QYUserInfo *)userInfo;
-
-/**
- *  设置authToken
- */
-- (void)setAuthToken:(NSString *)authToken;
-
-/**
- *  设置个人信息，带authToken校验。用户帐号登录成功之后，调用此函数
- *
- *  @param userInfo 个人信息
- *  @param block authToken校验结果的回调
- */
-- (void)setUserInfo:(QYUserInfo *)userInfo authTokenVerificationResultBlock:(QYCompletionWithResultBlock)block;
 
 /**
  *  获取推送消息
@@ -129,13 +191,6 @@ typedef void(^QYCleanCacheCompletion)(NSError *error);
  *  @param block 收到消息的回调
  */
 - (void)registerPushMessageNotification:(QYPushMessageBlock)block;
-
-/**
- *  返回AppKey
- *
- *  @return appKey
- */
-- (NSString *)appKey;
 
 /**
  *  访问轨迹
